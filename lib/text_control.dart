e640355172552a7757e97c6cb3989bfa7af268c2
lhamide <flashpointintl@gmail.com>
2018-12-26 14:33:55 +0100

    Completed My First Flutter Project

---
+++ lib/text_control.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import './text_output.dart';
+
+class TextControl extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TextControlState();
+  }
+}
+
+class _TextControlState extends State<TextControl> {
+
+  String _pageText = 'Hello World';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            color: Colors.yellowAccent,
+            onPressed: () {
+              setState(() {
+                this._pageText = (this._pageText == 'Hi Bro') ? 'Hello World' : 'Hi Bro';
+                print("Text Changed");
+              });
+            },
+            child: Text('Change Text'),
+          ),
+        ),
+        Center(
+          child: TextOutput(_pageText),
+        ),
+      ],
+    );
+  }
+}
